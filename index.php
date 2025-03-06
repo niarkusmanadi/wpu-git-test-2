646256c63a3aaab44501ac2732a105c24ab4ddc9
niar kusmanadi <niarkusmanadi@gmail.com>
2025-03-06 20:55:58 +0700

    menambahkan paragref

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hello World</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus excepturi reprehenderit iste eaque modi repudiandae error ipsa magnam ipsam placeat! Aut impedit voluptas culpa quam adipisci libero vel totam possimus!</p>
 </body>
 </html>
