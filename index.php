cd6243bb1ab3e0f56342ab5a165f97dc671f3014
niar kusmanadi <niarkusmanadi@gmail.com>
2025-03-06 21:01:29 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello World</h1>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus excepturi reprehenderit iste eaque modi repudiandae error ipsa magnam ipsam placeat! Aut impedit voluptas culpa quam adipisci libero vel totam possimus!</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
